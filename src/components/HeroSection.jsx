23338f316bba6f8de97a8a229703c121970bcb3a
Robbie213 <sasnarinedeodat407@gmail.com>
2024-02-16 16:42:17 -0500

    added the extract data button in the navbar

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -7,7 +7,7 @@ export default function HeroSection() {
         Scan your Driver's License and extract all details
       </h1>
       <img src={hero_image} alt="DL Extractor" className="w-2/3 mt-6" />
-      <button className="my-6 py-2 px-10 border-blue-500 border-2 w-fit text-blue-500 font-semibold text-xl bg-transparent rounded-md transition-all duration-300 hover:bg-blue-500 hover:text-white dark:hover:text-stone-900">
+      <button className="my-6 py-2 px-10 border-blue-500 border-2 w-fit text-blue-500 font-semibold text-xl bg-transparent rounded-md transition-all duration-300 hover:bg-blue-500 hover:text-white">
         Extract Data
       </button>
     </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,9 +2,14 @@ import logo from "../assets/logo.png";
 
 export default function Navbar() {
   return (
-    <div className="flex px-3 md:px-8 py-2">
+    <div className="flex px-3 md:px-8 py-2 items-center">
       <div className="flex-1">
         <img src={logo} alt="DL Extractor" />
+      </div>
+      <div>
+        <button className="py-1 px-6 w-fit text-white font-semibold text-lg bg-blue-500 rounded-md transition-all duration-300 hover:bg-blue-400">
+          Extract Data
+        </button>
       </div>
     </div>
   );
