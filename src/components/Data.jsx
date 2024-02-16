b8eb5354b7744451f1463806f33b32ae23f5b53d
Robbie213 <sasnarinedeodat407@gmail.com>
2024-02-16 17:33:35 -0500

    added the try again button on the data screen

--- src/components/Data.jsx
+++ src/components/Data.jsx
@@ -1,9 +1,16 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
+import { Button } from "@material-tailwind/react";
+import DialogBox from "./DialogBox";
 
 export default function Data() {
   const location = useLocation();
   const navigate = useNavigate();
+
+  const [open, setOpen] = useState(false);
+
+  const handleOpen = () => setOpen(!open);
+
   useEffect(() => {
     if (!location.state) {
       navigate("/");
@@ -43,6 +50,18 @@ export default function Data() {
           </li>
         </ul>
       </div>
+      <div className="flex items-center justify-center gap-4">
+        <p>Data Not Correct? </p>
+        <Button
+          className="my-6 py-1 px-6 border-blue-500 border-2 w-fit text-blue-500 font-semibold text-xl bg-transparent rounded-md transition-all duration-300 hover:bg-blue-500 hover:text-white capitalize"
+          data-ripple-light="true"
+          data-dialog-target="animated-dialog"
+          onClick={handleOpen}
+        >
+          Try Again
+        </Button>
+      </div>
+      <DialogBox open={open} handleOpen={handleOpen} />
     </div>
   );
 }
