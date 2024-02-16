fde263458b095c8c4773c5717a1832b0d06542b0
Robbie213 <sasnarinedeodat407@gmail.com>
2024-02-16 16:52:30 -0500

    created the webcam capture component to record the webcam

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import HeroSection from "./components/HeroSection";
 import Navbar from "./components/Navbar";
+import WebcamCapture from "./components/WebcamCapture";
 
 const App = () => {
   return (
     <div className="bg-white text-stone-900 dark:bg-stone-900 dark:text-white w-full h-full min-h-screen">
       <Navbar />
       <HeroSection />
+      <WebcamCapture />
     </div>
   );
 };
