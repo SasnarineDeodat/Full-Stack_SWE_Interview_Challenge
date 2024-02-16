d4cc0ffab0eac06ec07bc6d7d5577e769b088565
Robbie213 <sasnarinedeodat407@gmail.com>
2024-02-16 17:06:05 -0500

    setup react router dom for routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,24 @@
 import HeroSection from "./components/HeroSection";
 import Navbar from "./components/Navbar";
-import WebcamCapture from "./components/WebcamCapture";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Navbar />,
+    children: [
+      {
+        index: true,
+        element: <HeroSection />,
+      },
+    ],
+  },
+]);
 
 const App = () => {
   return (
     <div className="bg-white text-stone-900 dark:bg-stone-900 dark:text-white w-full h-full min-h-screen">
-      <Navbar />
-      <HeroSection />
-      <WebcamCapture />
+      <RouterProvider router={router} />
     </div>
   );
 };
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,6 +3,7 @@ import logo from "../assets/logo.png";
 import DarkMode from "./DarkMode";
 import { Button } from "@material-tailwind/react";
 import DialogBox from "./DialogBox";
+import { Outlet } from "react-router";
 
 export default function Navbar() {
   const [open, setOpen] = useState(false);
@@ -10,22 +11,25 @@ export default function Navbar() {
   const handleOpen = () => setOpen(!open);
 
   return (
-    <div className="flex px-3 md:px-8 py-2 items-center">
-      <div className="flex-1">
-        <img src={logo} alt="DL Extractor" />
+    <div>
+      <div className="flex px-3 md:px-8 py-2 items-center">
+        <div className="flex-1">
+          <img src={logo} alt="DL Extractor" />
+        </div>
+        <div>
+          <Button
+            className="py-1 px-6 w-fit text-white font-semibold text-lg bg-blue-500 rounded-md transition-all duration-300 hover:bg-blue-400 capitalize"
+            data-ripple-light="true"
+            data-dialog-target="animated-dialog"
+            onClick={handleOpen}
+          >
+            Extract Data
+          </Button>
+          <DialogBox open={open} handleOpen={handleOpen} />
+        </div>
+        <DarkMode />
       </div>
-      <div>
-        <Button
-          className="py-1 px-6 w-fit text-white font-semibold text-lg bg-blue-500 rounded-md transition-all duration-300 hover:bg-blue-400 capitalize"
-          data-ripple-light="true"
-          data-dialog-target="animated-dialog"
-          onClick={handleOpen}
-        >
-          Extract Data
-        </Button>
-        <DialogBox open={open} handleOpen={handleOpen} />
-      </div>
-      <DarkMode />
+      <Outlet />
     </div>
   );
 }
