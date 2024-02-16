e9dfd8075e50db7fde1034a1377f9d8958934678
Robbie213 <sasnarinedeodat407@gmail.com>
2024-02-16 16:34:32 -0500

    created an empty navbar component and added some tailwind classes to support both dark and light mode in future

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
+import Navbar from "./components/Navbar";
+
 const App = () => {
   return (
-    <div>
+    <div className="bg-white text-black dark:bg-black dark:text-white w-full h-full min-h-screen">
+      <Navbar />
       <h1>Interview Challenge</h1>
     </div>
   );
