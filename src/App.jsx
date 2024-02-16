cd1449e332ed544bf1d603a72aacd0dc4ba85f47
Robbie213 <sasnarinedeodat407@gmail.com>
2024-02-16 16:41:00 -0500

    created the hero section with a dummy image for now. the dummy image will be replaced with a proper image once the extraction section of the website is built

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
+import HeroSection from "./components/HeroSection";
 import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
     <div className="bg-white text-stone-900 dark:bg-stone-900 dark:text-white w-full h-full min-h-screen">
       <Navbar />
-      <h1>DL Extractor</h1>
+      <HeroSection />
     </div>
   );
 };
---
+++ src/components/HeroSection.jsx
@@ -0,0 +1,15 @@
+import hero_image from "../assets/hero_image.png";
+
+export default function HeroSection() {
+  return (
+    <div className="flex flex-col items-center">
+      <h1 className="text-4xl font-semibold text-center mt-4">
+        Scan your Driver's License and extract all details
+      </h1>
+      <img src={hero_image} alt="DL Extractor" className="w-2/3 mt-6" />
+      <button className="my-6 py-2 px-10 border-blue-500 border-2 w-fit text-blue-500 font-semibold text-xl bg-transparent rounded-md transition-all duration-300 hover:bg-blue-500 hover:text-white dark:hover:text-stone-900">
+        Extract Data
+      </button>
+    </div>
+  );
+}
