76b05f5457122a7932e2e1945811162775af7dab
Robbie213 <sasnarinedeodat407@gmail.com>
2024-02-16 17:33:03 -0500

    added the <ul> and <li> of where the extracted data will show

--- src/components/Data.jsx
+++ src/components/Data.jsx
@@ -14,9 +14,35 @@ export default function Data() {
       <h1 className="text-4xl font-semibold text-center mt-4">
         We have extracted Data from your Driver's License
       </h1>
-      {location.state && (
-        <img src={location.state?.image} alt="Your Drivers License" />
-      )}
+      <div className="flex justify-center mt-8">
+        {location.state && (
+          <img
+            className="max-w-xl rounded-md"
+            src={location.state?.image}
+            alt="Your Drivers License"
+          />
+        )}
+      </div>
+      <h1 className="text-4xl font-semibold text-center mt-4">Your Data</h1>
+      <div className="md:mx-14 mt-5">
+        <ul>
+          <li className="text-xl font-semibold pb-4">
+            Name: {location.state?.name}
+          </li>
+          <li className="text-xl font-semibold pb-4">
+            DOB: {location.state?.dob}
+          </li>
+          <li className="text-xl font-semibold pb-4">
+            Address: {location.state?.address}
+          </li>
+          <li className="text-xl font-semibold pb-4">
+            License Number: {location.state?.licenseNumber}
+          </li>
+          <li className="text-xl font-semibold pb-4">
+            Expiry Date: {location.state?.expiryDate}
+          </li>
+        </ul>
+      </div>
     </div>
   );
 }
