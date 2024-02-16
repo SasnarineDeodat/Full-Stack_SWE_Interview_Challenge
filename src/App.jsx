6e1bf2bb67ab99e1ba9f03700f2e42402ccd9c4c
Robbie213 <sasnarinedeodat407@gmail.com>
2024-02-16 16:37:42 -0500

    added a custom made logo in the navbar

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,9 @@ import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
-    <div className="bg-white text-black dark:bg-black dark:text-white w-full h-full min-h-screen">
+    <div className="bg-white text-stone-900 dark:bg-stone-900 dark:text-white w-full h-full min-h-screen">
       <Navbar />
-      <h1>Interview Challenge</h1>
+      <h1>DL Extractor</h1>
     </div>
   );
 };
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,11 @@
+import logo from "../assets/logo.png";
+
 export default function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <div className="flex px-3 md:px-8 py-2">
+      <div className="flex-1">
+        <img src={logo} alt="DL Extractor" />
+      </div>
+    </div>
+  );
 }
